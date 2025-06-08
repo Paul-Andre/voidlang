47c542394a3088d97771f495b0c6def17632636d
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2025-06-08 17:29:37 -0400

    terminal_colors.h full names

--- terminal_colors.h
+++ terminal_colors.h
@@ -2,11 +2,11 @@
 
 #define COLOR_RED   "\x1B[31m"
 #define COLOR_GREEN   "\x1B[32m"
-#define COLOR_YEL   "\x1B[33m"
-#define COLOR_BLU   "\x1B[34m"
-#define COLOR_MAG   "\x1B[35m"
-#define COLOR_CYN   "\x1B[36m"
-#define COLOR_WHT   "\x1B[37m"
+#define COLOR_YELLOW   "\x1B[33m"
+#define COLOR_BLUE   "\x1B[34m"
+#define COLOR_MAGENTA   "\x1B[35m"
+#define COLOR_CYAN   "\x1B[36m"
+#define COLOR_WHITE   "\x1B[37m"
 
 #define STYLE_BOLD "\033[1m"
 
